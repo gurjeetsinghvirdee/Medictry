9838ec6b5829bcac7c2111bfffdc1492fd262fae
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-09-12 19:51:23 +0530

    installed some dependencies and structured the project folder

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import './App.css';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <div className='App'>
+        
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
