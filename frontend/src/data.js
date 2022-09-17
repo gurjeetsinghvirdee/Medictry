5923a363bfeb626dbf9d1aa1c357d68d002c2249
Gurjeet Singh Virdee <73753957+gurjeetsinghvirdee@users.noreply.github.com>
2022-09-17 20:39:18 +0530

    Update data.js

--- frontend/src/data.js
+++ frontend/src/data.js
@@ -1,5 +1,5 @@
 const data = [
- medicines: [
+    meds: [
        {
           "Name": "Rabeprazole",
           "Image URL": "https://mediboonpharma.com/wp-content/uploads/2020/11/AAAAA_77.jpg",
@@ -45,68 +45,68 @@ const data = [
           "MFG": "2022",
           "EXPIRY": "2024"
        },
-{
-"Name": "Taxim-O 200 Tab",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/lt8bm4qbhvxnxtsfelcr.jpg",
-"Description": "axim-O 200 Tablet is an antibiotic belonging that is used to treat a variety of bacterial infections. It is effective in infections of the respiratory tract (eg. pneumonia), urinary tract, ear, nasal sinus, throat, and some sexually transmitted diseases.\r",
-"Orignal Price": "199.31",
-"Discounted Price": "95.45",
-"MFG": "2022",
-"EXPIRY": "2026"
-},
-{
-"Name": "Unienzyme Tablet",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/cropped/o6ww8zdgbvt3kh9unflu.png",
-"Description": "It is used to treat digestive problems. It is a dietary supplement that accelerates the digestion process and helps in the treatment of indigestion, bloating, gas or any stomach discomfort. It acts as a natural pro-digestive enzyme that helps in the breakdown of food into simpler compounds, thereby aiding the digestion process. The bacteria in probiotics also help to strengthen the body’s immune system and ward off infections. \r",
-"Orignal Price": "70.6",
-"Discounted Price": "60",
-"MFG": "2022",
-"EXPIRY": "2023"
-},
-{
-"Name": "Ultracet Tablet",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/eoreganmppqhojqzxb1i.jpg",
-"Description": "Ultracet Tablet is a pain-relieving medicine. It is used in various conditions such as muscle ache, back pain, joint pain, menstrual cramps, and toothache.",
-"Orignal Price": "50",
-"Discounted Price": "45",
-"MFG": "2022",
-"EXPIRY": "2026"
-},
-{
-"Name": "Urimax D Tablet MR",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/bv26drnqqrx2libpwmtq.jpg",
-"Description": "Urimax D Tablet MR is a combination of two medicines that work in different ways to treat men with an enlarged prostate gland. It helps to relieve symptoms such as difficulty urinating or needing to go to the toilet more often. It is not used to prevent prostate cancer.",
-"Orignal Price": "525",
-"Discounted Price": "420.5",
-"MFG": "2022",
-"EXPIRY": "2024"
-},
-{
-"Name": "Vitamin-C 500 Chewin Tablet",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/ygplr8ebymxk88bnbkur.jpg",
-"Description": "Vitamin-C 500 Chewable Tablet is essential for boosting your body's immunity. It helps to shield your body from various viruses. It is enriched with antioxidants that can strengthen your body’s natural defences. Antioxidants are molecules that boost the immune system by protecting cells from harmful molecules called free radicals. Vitamin C has an overall good impact on your health.",
-"Orignal Price": "48",
-"Discounted Price": "39",
-"MFG": "2022",
-"EXPIRY": "2024"
-},
-{
-"Name": "Vertin 8mg Tablet",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/ygihdgsvkrnwps0rpi0s.jpg",
-"Description": "Vertin 8mg Tablet is used to prevent and treat a disorder of the inner ear known as Ménière’s disease. The symptoms include dizziness (vertigo), ringing in the ears (tinnitus), and loss of hearing, probably caused by fluid in the ear. This medicine helps relieve the symptoms by reducing the amount of fluid.",
-"Orignal Price": "160",
-"Discounted Price": "128",
-"MFG": "2022",
-"EXPIRY": "2025"
-},
-{
-"Name": "Veltan 0.4 Tablet MR",
-"Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/ajdriehphdqgp1u3njx9.jpg",
-"Description": "Veltam 0.4 Tablet MR is an alpha-adrenergic antagonist that is used in the treatment of benign prostatic hyperplasia. It helps to relieve symptoms like difficulty in passing urine. However, it does not decrease the size of the prostate.",
-"Orignal Price": "171",
-"Discounted Price": "136",
-"MFG": "2022",
-"EXPIRY": "2024"
-}
-  ]
+       {
+          "Name": "Taxim-O 200 Tab",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/lt8bm4qbhvxnxtsfelcr.jpg",
+          "Description": "axim-O 200 Tablet is an antibiotic belonging that is used to treat a variety of bacterial infections. It is effective in infections of the respiratory tract (eg. pneumonia), urinary tract, ear, nasal sinus, throat, and some sexually transmitted diseases.\r",
+          "Orignal Price": "199.31",
+          "Discounted Price": "95.45",
+          "MFG": "2022",
+          "EXPIRY": "2026"
+       },
+       {
+          "Name": "Unienzyme Tablet",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/cropped/o6ww8zdgbvt3kh9unflu.png",
+          "Description": "It is used to treat digestive problems. It is a dietary supplement that accelerates the digestion process and helps in the treatment of indigestion, bloating, gas or any stomach discomfort. It acts as a natural pro-digestive enzyme that helps in the breakdown of food into simpler compounds, thereby aiding the digestion process. The bacteria in probiotics also help to strengthen the body’s immune system and ward off infections. \r",
+          "Orignal Price": "70.6",
+          "Discounted Price": "60",
+          "MFG": "2022",
+          "EXPIRY": "2023"
+       },
+       {
+          "Name": "Ultracet Tablet",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/eoreganmppqhojqzxb1i.jpg",
+          "Description": "Ultracet Tablet is a pain-relieving medicine. It is used in various conditions such as muscle ache, back pain, joint pain, menstrual cramps, and toothache.",
+          "Orignal Price": "50",
+          "Discounted Price": "45",
+          "MFG": "2022",
+          "EXPIRY": "2026"
+       },
+       {
+          "Name": "Urimax D Tablet MR",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/bv26drnqqrx2libpwmtq.jpg",
+          "Description": "Urimax D Tablet MR is a combination of two medicines that work in different ways to treat men with an enlarged prostate gland. It helps to relieve symptoms such as difficulty urinating or needing to go to the toilet more often. It is not used to prevent prostate cancer.",
+          "Orignal Price": "525",
+          "Discounted Price": "420.5",
+          "MFG": "2022",
+          "EXPIRY": "2024"
+       },
+       {
+          "Name": "Vitamin-C 500 Chewin Tablet",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_690,h_700/a_ignore,w_690,h_700,c_pad,q_auto,f_auto/ygplr8ebymxk88bnbkur.jpg",
+          "Description": "Vitamin-C 500 Chewable Tablet is essential for boosting your body's immunity. It helps to shield your body from various viruses. It is enriched with antioxidants that can strengthen your body’s natural defences. Antioxidants are molecules that boost the immune system by protecting cells from harmful molecules called free radicals. Vitamin C has an overall good impact on your health.",
+          "Orignal Price": "48",
+          "Discounted Price": "39",
+          "MFG": "2022",
+          "EXPIRY": "2024"
+       },
+       {
+          "Name": "Vertin 8mg Tablet",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/ygihdgsvkrnwps0rpi0s.jpg",
+          "Description": "Vertin 8mg Tablet is used to prevent and treat a disorder of the inner ear known as Ménière’s disease. The symptoms include dizziness (vertigo), ringing in the ears (tinnitus), and loss of hearing, probably caused by fluid in the ear. This medicine helps relieve the symptoms by reducing the amount of fluid.",
+          "Orignal Price": "160",
+          "Discounted Price": "128",
+          "MFG": "2022",
+          "EXPIRY": "2025"
+       },
+       {
+          "Name": "Veltan 0.4 Tablet MR",
+          "Image URL": "https://onemg.gumlet.io/l_watermark_346,w_480,h_480/a_ignore,w_480,h_480,c_fit,q_auto,f_auto/cropped/ajdriehphdqgp1u3njx9.jpg",
+          "Description": "Veltam 0.4 Tablet MR is an alpha-adrenergic antagonist that is used in the treatment of benign prostatic hyperplasia. It helps to relieve symptoms like difficulty in passing urine. However, it does not decrease the size of the prostate.",
+          "Orignal Price": "171",
+          "Discounted Price": "136",
+          "MFG": "2022",
+          "EXPIRY": "2024"
+       }
+   ]
 ]
